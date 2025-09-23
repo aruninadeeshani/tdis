ca9b5bc7f2bf1a181ab99ac6322bea632a610725
Dmitry Romanov <romanovda@gmail.com>
2025-09-23 11:18:24 -0400

    Looks deadend

---
+++ source/tdis/tracking/TrackingTypes.h
@@ -0,0 +1,9 @@
+//
+// Created by romanov on 9/23/2025.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#ifndef TDIS_ACTS_ROOT_TRACKINGTYPES_H
+#define TDIS_ACTS_ROOT_TRACKINGTYPES_H
+
+#endif //TDIS_ACTS_ROOT_TRACKINGTYPES_H
